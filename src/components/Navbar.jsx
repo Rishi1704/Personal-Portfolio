a133640168c8ce810c52a416a36a96b7f9d59afe
Rishi Agrawal <rishira21@iitk.ac.in>
2023-07-10 04:26:48 +0530

    Cleaned Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,8 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
 import { SiGmail } from 'react-icons/si'
 import { SlideBottom } from './slide-bottom'
 import { motion } from 'framer-motion'
-// import { GiHamburgerMenu } from 'react-icons/gi'
-
 
 const Navbar = () => {
-  // const [showMenuLinks, setShowMenuLinks] = useState(false);
-
   return (
     <>
       <motion.div className='sticky top-0 z-[99]'
@@ -24,38 +20,9 @@ const Navbar = () => {
           ease: "easeIn"
         }}
       >
-        {/* <SlideBottom delay={0}> */}
         <nav className='pointer-events-auto cursor-auto h-[70px] flex items-center justify-between
           bg-[#12121240] backdrop-blur-md w-full px-2 xs:px-4 sm:px-9 
           shadow-[0_50px_100px_-20px_rgba(250,250,193,0.25),0_30px_60px_-30px_rgba(255,255,255,0.3)]'>
-          {/* <div className='grid col-start-2 justify-start items-center'>
-            <Link to={'/'}>
-              <img className='h-[60px]' src='./Gamedev.png' alt="Studio Centauri" />
-            </Link>
-          </div> */}
-
-          {/* <div className='col-start-3 hidden lg:block'>
-            <ul className='h-full flex justify-around items-center'>
-              <li className='text-[20px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/'}>Home</Link>
-              </li>
-              <li className='text-[20px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/learn'}>Learn</Link>
-              </li>
-              <li className='text-[20px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' target={'_blank'} to={'https://studiocentauri.itch.io/'} rel="noreferrer">
-                  Play
-                </Link>
-              </li>
-              <li className='text-[20px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/blogs'}>Blogs</Link>
-              </li>
-              <li className='text-[20px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/contact'}>Contact Us</Link>
-              </li>
-            </ul>
-          </div> */}
-
           <div>
             <ul className='h-full grid grid-cols-[repeat(5,1fr)] justify-center items-center'>
               <SlideBottom delay={0.25}>
@@ -108,35 +75,6 @@ const Navbar = () => {
             </SlideBottom>
           </Link>
         </nav>
-
-        {/* <div className={showMenuLinks ? 'flex justify-center' : 'hidden'}>
-          <div className='pointer-events-auto cursor-auto lg:hidden block bg-[#1c1c1c] absolute w-[95%] origin-top z-[999] h-[200px] px-[30px]   
-          shadow-[0_50px_100px_-20px_rgba(250,250,193,0.25),0_30px_60px_-30px_rgba(255,255,255,0.3)]'>
-            <ul className='h-[200px] flex flex-col justify-around items-start'>
-              <li className='text-[17px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/'} 
-                onClick={() => setShowMenuLinks(!showMenuLinks)}>Home</Link>
-              </li>
-              <li className='text-[17px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/learn'} 
-                onClick={() => setShowMenuLinks(!showMenuLinks)}>Learn</Link>
-              </li>
-              <li className='text-[17px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'https://studiocentauri.itch.io/'} 
-                onClick={() => setShowMenuLinks(!showMenuLinks)}>Play</Link>
-              </li>
-              <li className='text-[17px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/blogs'} 
-                onClick={() => setShowMenuLinks(!showMenuLinks)}>Blogs</Link>
-              </li>
-              <li className='text-[17px] hover:scale-125 hover:font-semibold transition-all duration-300'>
-                <Link className='text-[rgba(255,255,255,0.8)]' to={'/contact'} 
-                onClick={() => setShowMenuLinks(!showMenuLinks)}>Contact Us</Link>
-              </li>
-            </ul>
-          </div>
-        </div> */}
-        {/* </SlideBottom> */}
       </motion.div>
     </>
   )
