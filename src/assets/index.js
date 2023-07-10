11480d9cd829884ddb6762868c93448405a8e42d
Rishi Agrawal <rishira21@iitk.ac.in>
2023-07-10 20:11:50 +0530

    Added Experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,8 @@
-// import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
-// import menu from "./menu.svg";
-// import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -21,24 +18,21 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import udghosh from "./company/udghosh.png";
+import gdc from "./company/gdc.png";
+import iitk from "./company/iitk.png";
+import vizuara from "./company/vizuara.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  // logo,
   backend,
   creator,
   mobile,
   web,
   github,
-  // menu,
-  // close,
   css,
   docker,
   figma,
@@ -52,10 +46,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  udghosh,
+  gdc,
+  iitk,
+  vizuara,
   carrent,
   jobit,
   tripguide,
