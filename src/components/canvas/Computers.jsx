21c640e3d4c21db15d7dc2c96a7f9e7f0e6f1cd5
Rishi Agrawal <rishira21@iitk.ac.in>
2023-07-10 06:40:23 +0530

    Updated Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,3 @@
-import * as THREE from 'three'
 import React, { Suspense, useEffect, useState} from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
