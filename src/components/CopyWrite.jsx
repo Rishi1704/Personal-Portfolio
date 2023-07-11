fd1ea3a0a9305e41bdb7b94ed84236233d44b3c3
Rishi Agrawal <rishira21@iitk.ac.in>
2023-07-11 17:43:24 +0530

    Copy write component

---
+++ src/components/CopyWrite.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CopyWrite = () => {
+    return (
+        <>
+            <p className='text-center bg-[#000000] text-[#0aff9d]'>
+                Rishi Agrawal &#169; 2023 - All Rights Reserved
+            </p>
+        </>
+    )
+}
+
+export default CopyWrite
